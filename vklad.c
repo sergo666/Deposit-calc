8336776553cccf34bd6d9261e3b375d5f2675c2c
sergo666 <serrrfff@gmail.com>
2017-02-16 21:40:53 +0700

    creat body

---
+++ vklad.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int srok,i;
+	float vklad;
+
+	printf("Срок:\n");        
+	scanf("%d",&srok);
+	printf("Сумма вклада:\n");
+	scanf("%f",&vklad);
+
+	return 0; 
+}
